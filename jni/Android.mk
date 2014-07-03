3fb7b483acea9e92b6b19c796fdfef6d2c533617
szymon <szymon@szymon-Lenovo-G570>
2014-07-04 00:08:21 +0200

    add settings to match https://gist.github.com/szymonrychu/54bb53638742759b8f9f

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 OPENCV_INSTALL_MODULES:=on
 
 
-include C:\Users\szymonri\DevTools\OpenCV-2.4.8-android-sdk\OpenCV-2.4.8-android-sdk\sdk\native\jni\OpenCV.mk
+include ${OPENCV_HOME}/sdk/native/jni/OpenCV.mk
 
 LOCAL_MODULE    := processing
 LOCAL_SRC_FILES := jni_part.cpp
